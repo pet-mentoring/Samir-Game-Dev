51bbac2a96096d145ce78623b989b826e821c54b
Peter Zhang <34387360+Ultrasword@users.noreply.github.com>
2024-01-09 21:55:38 -0500

    inputhandler handling the input from within game event loop

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pygame
 
 from scripts import entity
 from scripts import handler
+from scripts import inputhandler
 
 
 # create a window object
@@ -48,14 +49,29 @@ while running:
     # pause the game for a certain amount of time
     clock.tick(FPS)
 
+    inputhandler.update_hardware()
     # handle events
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             running = False
         elif e.type == pygame.KEYDOWN:
-            pass
+            # keyboard press
+            inputhandler.update_keyboard_down(e)
         elif e.type == pygame.KEYUP:
-            pass
+            # keyboard release
+            inputhandler.update_keyboard_up(e)
+        elif e.type == pygame.MOUSEMOTION:
+            # mouse movement
+            inputhandler.update_mouse_pos(e)
+        elif e.type == pygame.MOUSEWHEEL:
+            # mouse scroll
+            inputhandler.update_mouse_scroll(e)
+        elif e.type == pygame.MOUSEBUTTONDOWN:
+            # mouse press
+            inputhandler.update_mouse_press(e)
+        elif e.type == pygame.MOUSEBUTTONUP:
+            # mouse release
+            inputhandler.update_mouse_release(e)
 
     # draw everything
     window.fill(BACKGROUND)
